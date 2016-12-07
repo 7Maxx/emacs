7Maxx/emacs

commit 3352f61b4a615678c18b06056ced19d064ca663f

ibuffer-decompose-filter: Avoid side effects on error
* lisp/ibuf-ext.el (ibuffer-decompose-filter):
Update 'ibuffer-filtering-qualifiers' only if there is no error (Bug#24997).
* test/lisp/ibuffer-tests.el (ibuffer-test-Bug24997):
Update test result as pass.

lisp/ibuf-ext.el
@@ -929,26 +929,24 @@ This means that the topmost filter on the filtering stack, which must
 be a complex filter like (OR [name: foo] [mode: bar-mode]), will be
 turned into two separate filters [name: foo] and [mode: bar-mode]."
   (interactive)
-  (when (null ibuffer-filtering-qualifiers)
+  (unless ibuffer-filtering-qualifiers
     (error "No filters in effect"))
-  (let ((lim (pop ibuffer-filtering-qualifiers)))
-    (pcase (car lim)
-      (`or
-       (setq ibuffer-filtering-qualifiers (append
-					  (cdr lim)
-					  ibuffer-filtering-qualifiers)))
-      (`saved
-       (let ((data (assoc (cdr lim) ibuffer-saved-filters)))
-	 (unless data
-	   (ibuffer-filter-disable)
-	   (error "Unknown saved filter %s" (cdr lim)))
-	 (setq ibuffer-filtering-qualifiers
-               (append (cdr data) ibuffer-filtering-qualifiers))))
-      (`not
-       (push (cdr lim)
-	     ibuffer-filtering-qualifiers))
-      (_
-       (error "Filter type %s is not compound" (car lim)))))
+  (let* ((filters ibuffer-filtering-qualifiers)
+         (head (cdar filters))
+         (tail (cdr filters))
+         (value
+          (pcase (caar filters)
+            (`or (nconc head tail))
+            (`saved
+             (let ((data (assoc head ibuffer-saved-filters)))
+               (unless data
+                 (ibuffer-filter-disable)
+                 (error "Unknown saved filter %s" head))
+               (append (cadr data) tail)))
+            (`not (cons head tail))
+            (_
+             (error "Filter type %s is not compound" (caar filters))))))
+    (setq ibuffer-filtering-qualifiers value))
   (ibuffer-update nil t))
 
 ;;;###autoload

test/lisp/ibuffer-tests.el
@@ -34,7 +34,6 @@
 
 (ert-deftest ibuffer-test-Bug24997 ()
   "Test for http://debbugs.gnu.org/24997 ."
-  :expected-result :failed
   (ibuffer)
   (let ((orig ibuffer-filtering-qualifiers))
     (unwind-protect

commit d1b2118dd5d9ee739f1e92d92e203e1425f9549b

Fix regression introduced by commit 3352f61
* lisp/ibuf-ext.el (ibuffer-decompose-filter): Use cdr instead
of cadr; required after commit dadb115.

lisp/ibuf-ext.el
@@ -941,7 +941,7 @@ turned into two separate filters [name: foo] and [mode: bar-mode]."
                (unless data
                  (ibuffer-filter-disable)
                  (error "Unknown saved filter %s" head))
-               (append (cadr data) tail)))
+               (append (cdr data) tail)))
             (`not (cons head tail))
             (_
              (error "Filter type %s is not compound" (caar filters))))))